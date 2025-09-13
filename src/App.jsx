ae4ce38253ebabcb37f531f4a529a06e2533a6e1
Demetreius <tchendjedimitri@gmail.com>
2025-09-13 08:18:09 +0100

    refactoring the repo with github

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <div className='h-[100vh] flex-center'>
+            <h1 className="text-7xl">Hello, GSAP !</h1>
+        </div>
+    )
+}
+
+export default App
